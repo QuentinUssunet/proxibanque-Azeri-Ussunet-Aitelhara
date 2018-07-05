d1e7fe2e17dda8cc3de1afc2babda0209c8ef4e3
quentinussunet <qussunet@gmail.com>
2018-07-05 15:37:31 +0200

    update

--- src/main/java/fr/gtm/projetv3/service/ClientService.java
+++ src/main/java/fr/gtm/projetv3/service/ClientService.java
@@ -3,12 +3,12 @@ package fr.gtm.projetv3.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fr.gtm.projetv3.dao.ClientRepository;
 import fr.gtm.projetv3.model.Client;
 import fr.gtm.projetv3.model.Compte;
-import fr.gtm.projetv3.model.Results;
 
 public class ClientService {
 
@@ -16,7 +16,7 @@ public class ClientService {
 	ClientRepository repo;
 	
 	@Autowired
-	Results res;
+	ObjectFactory<Results> res;
 		
 	// Affichage des comptes du client
 	public List<Compte> listComptes(Integer idClient){
@@ -26,14 +26,22 @@ public class ClientService {
 	
 	// Authentification du client par son nom
 	public Client findByName(String nom) {
-		this.res.setNom(nom);
-		return this.repo.getByName(nom);
+		return this.res.getObject().setNom(nom);
 	}
 	
-	public Client findByFirstName(String prenom) {
-		this.res.setPrenom(prenom);
-		return this.repo.getByFirstName(prenom);
+	// Authentification du client par son prenom
+	public Client findByFirstName(String prenom) {		
+		return this.res.getObject().setPrenom(prenom);
 	}
 	
-	
+	// Methode de comparaison client par nom, client par prenom
+	public Client compare(Client c1,Client c2) {
+		Client result = null;
+		if(c1.equals(c2)) {
+			result = c1;
+		} else {
+			// TODO message client combinaison nom+prenom inexistant veuillez ressaisir
+		}
+		return result;
+	}
 }
--- src/main/java/fr/gtm/projetv3/service/Results.java
+++ src/main/java/fr/gtm/projetv3/service/Results.java
@@ -1,4 +1,9 @@
-package fr.gtm.projetv3.model;
+package fr.gtm.projetv3.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.gtm.projetv3.dao.ClientRepository;
+import fr.gtm.projetv3.model.Client;
 
 public class Results {
 
@@ -6,6 +11,11 @@ public class Results {
 	private String nom;
 	private String prenom;
 	
+	@Autowired
+	ClientRepository repo;
+	
+	
+
 	
 	// Constructeur par défault
 	
@@ -23,14 +33,16 @@ public class Results {
 	public String getNom() {
 		return nom;
 	}
-	public void setNom(String nom) {
+	public Client setNom(String nom) {
 		this.nom = nom;
+		return this.repo.getAllByName(nom);
 	}
 	public String getPrenom() {
 		return prenom;
 	}
-	public void setPrenom(String prenom) {
+	public Client setPrenom(String prenom) {
 		this.prenom = prenom;
+		return this.repo.getAllByFirstName(prenom);
 	}
 	
 }
